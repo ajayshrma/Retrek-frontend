50523933f6e575eb0f43f71d96583fb897f40557
ajayshrma <ajaysharma27600@gmail.com>
2025-10-06 00:07:40 +0530

    first commit

---
+++ retrek-frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello World 👋</h1>
+      <p>Welcome to Retrek!</p>
+    </div>
+  );
+}
+
+export default Home;
